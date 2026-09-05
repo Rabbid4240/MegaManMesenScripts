b6b9e82ad7459388ef52d76b9347883a3b169ce3
Rabbid4240 <144391703+Rabbid4240@users.noreply.github.com>
2026-09-05 19:02:37 -0400

    Update MM1Hotswap.lua

--- MM1Hotswap.lua
+++ MM1Hotswap.lua
@@ -1,4 +1,4 @@
-﻿-- Quick Weapon Swapping (MM10) for Mega Man
+﻿-- Quick Weapon Swapping (MM10) for Mega Man 1
 -- by: Rabbid4240
 
 -- For use with MesenCE, compatability might break with other versions.
